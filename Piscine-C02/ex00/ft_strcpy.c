c6255074cbd2f1fb805e12194c6d17c60a918849
andemart <andemart@student.42urduliz.com>
2024-06-19 15:23:10 +0200

    Piscine-C02-ex00

---
+++ Piscine-C02/ex00/ft_strcpy.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andemart <andemart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 14:09:04 by andemart          #+#    #+#             */
+/*   Updated: 2024/06/19 14:42:23 by andemart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int		i;
+
+	i = 0;
+	while (src[i] != '\0' && n > i)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*int main(void){
+    char origen[] = "Hola, Mundo!";
+    char destino[20];
+
+    printf("Origen: %s\n", origen);
+
+    ft_strcpy(destino, origen, 6);
+
+    printf("Destino: %s\n", destino);
+}*/
